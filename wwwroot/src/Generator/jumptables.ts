fc721339740c08ad979f42a1bddd82d5c5718ade
itim <itim@kobkiat-it.com>
2017-03-10 11:51:15 +0700

    add test

--- wwwroot/src/Generator/jumptables.ts
+++ wwwroot/src/Generator/jumptables.ts
@@ -30,5 +30,50 @@ namespace Generator {
         public get maxJumpVelocity(): number {
             return this._jumpVelocities[this._jumpVelocities.length - 1];
         }
+
+        // ------------------
+        // ------ DEBUG -----
+        // ------------------
+        private static _DEBUG = false;
+        private static _globals: any;
+        private static _debugBmd: Phaser.BitmapData;
+
+        public static setDebug(debug: boolean, gameGlobals?: any): void {
+            JumpTables._DEBUG = debug;
+            JumpTables._globals = gameGlobals;
+
+            if (debug) {
+                if (typeof gameGlobals == 'undefined' || gameGlobals == null) {
+                    console.warn("No game globals provide - switching debug off");
+                } else {
+                    JumpTables.createDebugBitmap();
+                }
+            }
+        }
+
+        public static get debugBitmapData(): Phaser.BitmapData {
+            return JumpTables._debugBmd;
+        }
+
+        private static createDebugBitmap(): void {
+            let global = JumpTables._globals;
+
+            let bmd = new Phaser.BitmapData(global.game, "Grid", global.GAME_WIDTH, global.GAME_HEIGHT);
+            bmd.fill(192, 192, 192);
+
+            // horizontal lines
+            for (let i = 0; i < global.GAME_HEIGHT; i += Parameters.CELL_SIZE) {
+                bmd.line(0, i + 0.5, global.GAME_WIDTH - 1, i + 0.5);
+            }
+
+            // vertical lines
+            for (let i = 0; i < global.GAME_WIDTH; i += Parameters.CELL_SIZE) {
+                bmd.line(i + 0.5, 0, i + 0.5, global.GAME_HEIGHT - 1);
+                // add columns header numbers
+                bmd.text("" + (i / Parameters.CELL_SIZE), i + 20, 20, "24px Courier", "#FFFF00");
+            }
+
+            JumpTables._debugBmd = bmd;
+        }
     }
 }
